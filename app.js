688ccaffa5f5d6be7edfca0d7a19c54f82c57c33
acenorth <north.andy@gmail.com>
2016-11-08 11:45:23 -0600

    nunjucks integration

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
+const volleyball = require('volleyball');
+const router = express.Router();
+const nunjucks = require('nunjucks');
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
 
-app.get('/', function(req, res) {
-	res.send("Welcome to our Twitter!")
+app.use(volleyball);
+app.use('/', router);
+
+
+// var coolPeepsPage = {
+// 	"title" : "An Example",
+// 	"people" : [
+// 	{name: "Gandalf"}, {name: "Frodo"}, {name: "Hermione"}
+// 	]
+// }
+// var rendered = nunjucks.render('index.html', coolPeepsPage);
+
+router.get('/', function(req, res) {
+	res.send("Welcome to our twitter, Gandalf");
+})
+
+router.get('/news', function(req, res) {
+	res.send("This is our news page.");
 })
 
 app.listen(3000, function() {
-	console.log("We are listening!")
+	console.log("We are listening!");
+
 });
